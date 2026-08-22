355577a269570bb9a4a435629260f844df35c83c
Maryam <mannorajpoot003@gmail.com>
2026-08-22 12:35:23 +0500

    Allow all hosts in settings for PythonAnywhere deployment

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6eme6qdhyoj2ve4p*77%xptmc6h&t@yb1r-#-%*oy(vjstux62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
